6ab4ca99b596a5e06b4bb04d6a90edf85cd209e8
David Stark <david.stark@zarkonnen.com>
2012-11-09 18:32:14 +0100

    Work on new Selenium 2 language exporter code.

---
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/io/formats/csharp_new_nunit.js
@@ -0,0 +1,35 @@
+builder.selenium2.io.addDerivedLangFormatter("C#", {
+  name: "C#/NUnit",
+  start:
+    "using OpenQA.Selenium;\n" +
+    "using OpenQA.Selenium.Remote;\n" +
+    "using OpenQA.Selenium.Support.UI;\n"+
+    "using System;\n" +
+    "using NUnit.Framework;\n"
+    "\n" +
+    "namespace se_builder {\n" +
+    "  [TestFixture()]\n" +
+    "  public class {name} {\n" +
+    "    [Test()]\n" +
+    "    static void TestCase() {\n" +
+	  "      IWebDriver wd = new RemoteWebDriver(DesiredCapabilities.Firefox());\n" +
+	  "        try {\n",
+  end:
+    "        } finally { wd.Quit(); }\n" +
+    "    }\n" +
+    "  }\n}\n",
+  assert: function(step, escapeValue, doSubs, getter) {
+    if (step.negated) {
+      return "        Assert.AreNotEqual(" + doSubs(getter.getter) + ", " + doSubs(getter.cmp) + ");\n";
+    } else {
+      return "        Assert.AreEqual(" + doSubs(getter.getter) + ", " + doSubs(getter.cmp) + ");\n";
+    }
+  },
+  boolean_assert: function(step, escapeValue, doSubs, getter) {
+    if (step.negated) {
+      return "        Assert.IsFalse(" + doSubs(getter.getter) + ");\n";
+    } else {
+      return "        Assert.IsTrue(" + doSubs(getter.getter) + ");\n";
+    }
+  }
+});
--- seleniumbuilder/chrome/content/html/js/builder/selenium2/io/io.js
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/io/io.js
@@ -114,6 +114,66 @@ builder.selenium2.io.saveScriptWithParams = function(script, format, path, param
 
 builder.selenium2.io.formats = [];
 
+builder.selenium2.io.makeDoSubs = function(script, name, userParams, used_vars, lang_info) {
+  return function(line) {
+    for (var k in userParams) {
+      line = line.replace("{" + k + "}", userParams[k]);
+    }
+    var pNames = script.steps[i].getParamNames();
+    for (var j = 0; j < pNames.length; j++) {
+      if (step.type.getParamType(pNames[j]) == "locator") {
+        line = line.replace(new RegExp("\\{" + pNames[j] + "\\}", "g"), lang_info.escapeValue(step.type, step[pNames[j]].getValue(), pNames[j]));
+        line = line.replace(new RegExp("\\{" + pNames[j] + "By\\}", "g"), lang_info.locatorByForType(step.type, step[pNames[j]].getName(builder.selenium2), j + 1));
+      } else {
+        line = line.replace(new RegExp("\\{" + pNames[j] + "\\}", "g"), lang_info.escapeValue(step.type, step[pNames[j]], pNames[j]));
+      }
+    }
+    // Depending on whether the step is negated, put in the appropriate logical nots.
+    if (step.negated) {
+      line = line.replace(new RegExp("\\{posNot\\}", "g"), "");
+      line = line.replace(new RegExp("\\{negNot\\}", "g"), lang_info.not);
+    } else {
+      line = line.replace(new RegExp("\\{posNot\\}", "g"), lang_info.not);
+      line = line.replace(new RegExp("\\{negNot\\}", "g"), "");
+    }
+    // Replace ${foo} with the necessary invocation of the variable, eg "String foo" or "var foo".
+    var l2 = "";
+    var hasDollar = false;
+    var insideVar = false;
+    var varName = "";
+    for (var j = 0; j < line.length; j++) {
+      var ch = line.substring(j, j + 1);
+      if (insideVar) {
+        if (ch == "}") {
+          var spl = varName.split(":", 2);
+          var varType = spl.length == 2 ? spl[1] : null;
+          varName = spl[0];
+          if (used_vars[varName]) {
+            l2 += lang_info.usedVar(varName, varType);
+          } else {
+            l2 += lang_info.unusedVar(varName, varType);
+            used_vars[varName] = true;
+          }
+          insideVar = false;
+          hasDollar = false;
+          varName = "";
+        } else {
+          varName += ch;
+        }
+      } else {
+        // !insideVar
+        if (hasDollar) {
+          if (ch == "{") { insideVar = true; } else { hasDollar = false; l2 += "$" + ch; }
+        } else {
+          if (ch == "$") { hasDollar = true; } else { l2 += ch; }
+        }
+      }
+    }
+    
+    return l2;
+  };
+};
+
 builder.selenium2.io.createLangFormatter = function(lang_info) {
   return {
     name: lang_info.name,
@@ -129,71 +189,29 @@ builder.selenium2.io.createLangFormatter = function(lang_info) {
       var used_vars = {};
       for (var i = 0; i < script.steps.length; i++) {
         var step = script.steps[i];
+        var doSubs = builder.selenium2.io.makeDoSubs(script, name, userParams, used_vars, lang_info);
         var line = lang_info.lineForType[step.type.name];
-        if (typeof line == 'undefined') {
+        if (typeof line != 'undefined') {
+          if (line instanceof Function) {
+            t += line(step, lang_info.escapeValue, userParams, doSubs);
+            continue;
+          } else {
+            t += doSubs(line);
+          }
+        } else {
+          var booleanVersion = false;
+          lp: for (int b = 0; b < 2; b++) {
+            var stepFlavors = ["assert", "verify", "waitFor", "store"];
+            booleanVersion = true;
+          }
           throw(_t('sel2_cant_export_step_type', step.type.name));
         }
-        if (line instanceof Function) {
-          t += line(step, lang_info.escapeValue, userParams);
-          continue;
-        }
-        for (var k in userParams) {
-          line = line.replace("{" + k + "}", userParams[k]);
-        }
-        var pNames = script.steps[i].getParamNames();
-        for (var j = 0; j < pNames.length; j++) {
-          if (step.type.getParamType(pNames[j]) == "locator") {
-            line = line.replace(new RegExp("\\{" + pNames[j] + "\\}", "g"), lang_info.escapeValue(step.type, step[pNames[j]].getValue(), pNames[j]));
-            line = line.replace(new RegExp("\\{" + pNames[j] + "By\\}", "g"), lang_info.locatorByForType(step.type, step[pNames[j]].getName(builder.selenium2), j + 1));
-          } else {
-            line = line.replace(new RegExp("\\{" + pNames[j] + "\\}", "g"), lang_info.escapeValue(step.type, step[pNames[j]], pNames[j]));
-          }
-        }
-        // Depending on whether the step is negated, put in the appropriate logical nots.
-        if (step.negated) {
-          line = line.replace(new RegExp("\\{posNot\\}", "g"), "");
-          line = line.replace(new RegExp("\\{negNot\\}", "g"), lang_info.not);
-        } else {
-          line = line.replace(new RegExp("\\{posNot\\}", "g"), lang_info.not);
-          line = line.replace(new RegExp("\\{negNot\\}", "g"), "");
-        }
-        // Replace ${foo} with the necessary invocation of the variable, eg "String foo" or "var foo".
-        var l2 = "";
-        var hasDollar = false;
-        var insideVar = false;
-        var varName = "";
-        for (var j = 0; j < line.length; j++) {
-          var ch = line.substring(j, j + 1);
-          if (insideVar) {
-            if (ch == "}") {
-              var spl = varName.split(":", 2);
-              var varType = spl.length == 2 ? spl[1] : null;
-              varName = spl[0];
-              if (used_vars[varName]) {
-                l2 += lang_info.usedVar(varName, varType);
-              } else {
-                l2 += lang_info.unusedVar(varName, varType);
-                used_vars[varName] = true;
-              }
-              insideVar = false;
-              hasDollar = false;
-              varName = "";
-            } else {
-              varName += ch;
-            }
-          } else {
-            // !insideVar
-            if (hasDollar) {
-              if (ch == "{") { insideVar = true; } else { hasDollar = false; l2 += "$" + ch; }
-            } else {
-              if (ch == "$") { hasDollar = true; } else { l2 += ch; }
-            }
-          }
-        }
-        line = l2;
-        t += line;
       }
-      t += lang_info.end;
+      var end = lang_info.start.replace(/\{name\}/g, name.substr(0, name.indexOf(".")));
+      for (var k in userParams) {
+        end = end.replace("{" + k + "}", userParams[k]);
+      }
+      t += end;
       return t;
     },
     canExport: function(stepType) {

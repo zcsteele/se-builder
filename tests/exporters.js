1246f38bce06ecc2c77ea621a6351f6cb85e7a17
berni <berni@extensa.pl>
2013-01-22 16:31:46 +0100

    Travis CI integration, automatic converter
    else on its own line

---
+++ tests/exporters.js
@@ -0,0 +1,23 @@
+// test exporters - run selenium tests using each one
+
+var assert = require('assert')
+  , convert = require('../tools/convert');
+
+// test exporters
+var exporters = ["node-wd", "java_new"];
+var testExporter = function(exporter, testFile) {
+  assert.doesNotThrow(function(){
+    convert.run([exporter, testFile]);
+  });
+};
+
+var testFile = "./tests/examples/full_example.json";
+
+for (var i = exporters.length - 1; i >= 0; i--) {
+  testExporter(exporters[i], testFile);
+}
+
+// non-available exporter
+assert.throws(function() {
+  convert.run(["non-available-exporter", testFile]);
+});

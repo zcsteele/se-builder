c093d39f25d099f63f1b317966b04c80ea39d2a0
Zarkonnen <david.stark@zarkonnen.com>
2014-02-14 10:49:53 +0100

    Removed dependency on bridge for script.js. Hopefully fixes the build. Issue #67 .

--- seleniumbuilder/chrome/content/html/js/builder/script.js
+++ seleniumbuilder/chrome/content/html/js/builder/script.js
@@ -1,5 +1,5 @@
 builder.breakpointsEnabled = true;
-builder.shareSuiteState = bridge.prefManager.getBoolPref("extensions.seleniumbuilder.shareSuiteState");
+builder.shareSuiteState = bridge ? bridge.prefManager.getBoolPref("extensions.seleniumbuilder.shareSuiteState") : false;
 
 /**
  * Defines a Script object that encapsulates a single test script.
